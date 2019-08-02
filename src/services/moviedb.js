c4a3dbaa01d8cd1ba69909ffb388a383006d91d1
cortl <cortlan.bainbridge@gmail.com>
2019-08-02 14:14:53 -0500

    service cleanup

--- src/services/moviedb.js
+++ src/services/moviedb.js
@@ -1,26 +1,19 @@
 import config from 'config';
 import Axios from 'axios';
 
-// Axios.interceptors.request.use(request => {
-//     console.log('Starting Request', request.url);
-//     return request;
-// });
-
-// Axios.interceptors.response.use(response => {
-//     console.log('Response:', response);
-//     return response;
-// });
-
 export const getTvDetails = tvId =>
     Axios.get(`https://api.themoviedb.org/3/tv/${tvId}?api_key=${config.get('API_KEY')}&language=en-US`)
-        .then(res => res.data);
+        .then(res => res.data)
+        .catch(err => {
+            console.log(`Error trying to get tv show details for ${tvId} with error ${err.response.status}: ${err.message}`);
+            throw err;
+        });
         
 export const getSeasonDetails = (tvId, seasonNumber) =>
     Axios.get(`https://api.themoviedb.org/3/tv/${tvId}/season/${seasonNumber}?api_key=${config.get('API_KEY')}&language=en-US`)
         .then(res => res.data)
         .catch(err => {
-            console.log(`Error trying to get season ${seasonNumber} from TV Show ${tvId}`);
-            console.error(err.message);
+            console.log(`Error trying to get season ${seasonNumber} from TV Show ${tvId} with error ${err.response.status}: ${err.message}`);
             throw err;
         });
 
@@ -28,7 +21,6 @@ export const getEpisodeDetails = (tvId, seasonNumber, episodeNumber) =>
     Axios.get(`https://api.themoviedb.org/3/tv/${tvId}/season/${seasonNumber}/episode/${episodeNumber}?api_key=${config.get('API_KEY')}&language=en-US`)
         .then(res => res.data)
         .catch(err => {
-            console.log(`Error trying to get episode ${episodeNumber} from TV Show ${tvId}, Season ${seasonNumber}`);
-            console.error(err.message);
+            console.log(`Error trying to get episode ${episodeNumber} from TV Show ${tvId}, Season ${seasonNumber} with error ${err.response.status}: ${err.message}`);
             throw err;
         });
