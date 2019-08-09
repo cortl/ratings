3bea8f9d9fb0ac8f79cab29bc11abb285449cfb5
cortl <cortlan.bainbridge@gmail.com>
2019-08-09 15:16:23 -0500

    adding get external ids

--- src/services/moviedb.js
+++ src/services/moviedb.js
@@ -8,7 +8,7 @@ export const getTvDetails = tvId =>
             console.log(`Error trying to get tv show details for ${tvId} with error ${err.response.status}: ${err.message}`);
             throw err;
         });
-        
+
 export const getSeasonDetails = (tvId, seasonNumber) =>
     Axios.get(`https://api.themoviedb.org/3/tv/${tvId}/season/${seasonNumber}?api_key=${config.get('API_KEY')}&language=en-US`)
         .then(res => res.data)
@@ -22,5 +22,13 @@ export const getEpisodeDetails = (tvId, seasonNumber, episodeNumber) =>
         .then(res => res.data)
         .catch(err => {
             console.log(`Error trying to get episode ${episodeNumber} from TV Show ${tvId}, Season ${seasonNumber} with error ${err.response.status}: ${err.message}`);
+            throw err;
+        });
+
+export const getExternalIds = (tvId) =>
+    Axios.get(`https://api.themoviedb.org/3/tv/${tvId}/external_ids?api_key=${config.get('API_KEY')}&language=en-US`)
+        .then(res => res.data)
+        .catch(err => {
+            console.log(`Error trying to external ids from TV Show ${tvId} with error ${err.response.status}: ${err.message}`);
             throw err;
         });
